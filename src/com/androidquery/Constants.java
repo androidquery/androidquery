0233bb29496d1ecb5807944aa71b337571d01406
tinyeeliu@gmail.com <tinyeeliu@gmail.com@d864b24f-c464-86f1-a23f-96be1521e7f2>
2011-05-23 14:38:28 +0000

    git-svn-id: https://android-query.googlecode.com/svn/trunk@12 d864b24f-c464-86f1-a23f-96be1521e7f2

--- src/com/androidquery/Constants.java
+++ src/com/androidquery/Constants.java
@@ -5,4 +5,5 @@ public interface Constants {
 	public final int LAYER_TYPE_SOFTWARE = 1;
 	public final int LAYER_TYPE_HARDWARE = 2;
 	public final int FLAG_HARDWARE_ACCELERATED = 0x01000000;
+	public final int FLAG_ACTIVITY_NO_ANIMATION = 0x00010000;
 }
