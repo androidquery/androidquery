3ea97af3568d244a2131d03150ed90842b9af487
tinyeeliu@gmail.com <tinyeeliu@gmail.com@d864b24f-c464-86f1-a23f-96be1521e7f2>
2011-10-01 15:42:04 +0000

    git-svn-id: https://android-query.googlecode.com/svn/trunk@620 d864b24f-c464-86f1-a23f-96be1521e7f2

---
+++ test/com/androidquery/test/image/ImageLoadingListOptionsActivity.java
@@ -0,0 +1,155 @@
+package com.androidquery.test.image;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ImageView.ScaleType;
+import android.widget.Toast;
+
+import com.androidquery.AQuery;
+import com.androidquery.R;
+import com.androidquery.TQuery;
+import com.androidquery.callback.AjaxStatus;
+import com.androidquery.callback.BitmapAjaxCallback;
+import com.androidquery.test.RunSourceActivity;
+import com.androidquery.util.AQUtility;
+import com.androidquery.util.XmlDom;
+
+public class ImageLoadingListOptionsActivity extends ImageLoadingListActivity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+	protected int getContainer(){
+		return R.layout.image_list_options_activity;
+	}
+	
+	public void work(){
+	    
+		
+		AQUtility.cleanCacheAsync(this, 0, 0);
+		BitmapAjaxCallback.clearCache();
+		
+        String url = "https://picasaweb.google.com/data/feed/base/featured?max-results=48";
+		aq.progress(R.id.progress).ajax(url, XmlDom.class, this, "renderPhotos");
+	     
+		
+	}
+	
+	private List<Photo> convertAll(XmlDom xml){
+		
+		List<XmlDom> entries = xml.children("entry");
+		
+		List<Photo> result = new ArrayList<Photo>();
+		
+		for(XmlDom entry: entries){
+			result.add(convert(entry));
+		}
+		
+		return result;
+	}
+	
+	private Photo convert(XmlDom xml){
+		
+		String url = xml.child("content").attr("src");
+		String title = xml.child("title").text();
+		String author = xml.child("author").text("name");
+		
+		String tb = url;
+		List<XmlDom> tbs = xml.tags("media:thumbnail");
+		
+		if(tbs.size() > 0){
+			tb = tbs.get(0).attr("url");
+			//tb = tbs.get(tbs.size() - 1).attr("url");
+		}
+		
+		tb = tb.replaceAll("https:", "http:");
+		
+		Photo photo = new Photo();
+		photo.url = url;
+		photo.tb = tb;
+		photo.title = title;
+		photo.author = author;
+		
+		return photo;
+	}
+	
+	public void scrolledBottom(AbsListView view, int scrollState){
+		
+		Toast toast = Toast.makeText(this, "ScrolledBottom", Toast.LENGTH_SHORT);
+		toast.show();
+	}
+	
+	public void renderPhotos(String url, XmlDom xml, AjaxStatus status) {
+	
+		if(xml == null) return;
+		
+		List<Photo> entries = convertAll(xml);
+	
+		ArrayAdapter<Photo> aa = new ArrayAdapter<Photo>(this, R.layout.photo_item, entries){
+			
+			public View getView(int position, View convertView, ViewGroup parent) {
+				
+				if(convertView == null){
+					convertView = getLayoutInflater().inflate(R.layout.photo_item, parent, false);
+				}
+				
+				Photo photo = getItem(position);
+				
+				AQuery aq = new AQuery(convertView);
+				
+				aq.id(R.id.name).text(photo.title);
+				aq.id(R.id.meta).text(photo.author);
+				
+				String tbUrl = photo.tb;
+				
+				Bitmap placeholder = aq.getCachedImage(R.drawable.image_ph);
+				
+				if(aq.shouldDelay(convertView, parent, tbUrl, 0)){
+					aq.id(R.id.tb).image(placeholder);
+				}else{
+					aq.id(R.id.tb).image(tbUrl, true, true, 0, 0, placeholder, 0, 0);
+				}
+				
+				return convertView;
+				
+			}
+			
+			
+		};
+		
+		aq.id(R.id.list).adapter(aa);
+		
+	}
+	
+		
+	class Photo{
+		
+		String tb;
+		String url;
+		String title;
+		String author;
+	}
+	
+	
+}
