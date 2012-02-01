dfa5a16393df0f3be3e57ff08d32d45e5f4c32a9
peter <tinyeeliu@gmail.com>
2012-02-01 14:35:18 +0800

    Organize imports.

--- src/com/androidquery/callback/AbstractAjaxCallback.java
+++ src/com/androidquery/callback/AbstractAjaxCallback.java
@@ -32,7 +32,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
