4d11ccc8b3e39065daba2fce6f2e2ff235ff249b
peter <tinyeeliu@gmail.com>
2012-04-02 02:00:37 +0800

    Move lib to libs to work around ADT missing lib issue.

--- demo/src/com/androidquery/test/image/ImageLoadingList4Activity.java
+++ demo/src/com/androidquery/test/image/ImageLoadingList4Activity.java
@@ -9,7 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.AbsListView.OnScrollListener;
 
 import com.androidquery.AQuery;
 import com.androidquery.R;
@@ -90,6 +95,115 @@ public class ImageLoadingList4Activity extends ImageLoadingListActivity {
 		toast.show();
 	}
 	
+	private boolean shouldDelay(int position, View convertView, ViewGroup parent, String url){
+		
+		Bitmap bm = BitmapAjaxCallback.getMemoryCached(url, 0);
+		if(bm != null) return false;
+		
+		ListView lv = (ListView) parent;
+		
+		OnScrollListener sl = (OnScrollListener) parent.getTag(AQuery.TAG_SCROLL_LISTENER);
+		
+		if(sl == null){
+		
+			sl = new OnScrollListener() {
+			
+				@Override
+				public void onScrollStateChanged(AbsListView view, int scrollState) {
+					
+					//ExpandableListView elv = (ExpandableListView) view;
+					ListView lv = (ListView) view;
+					
+					view.setTag(AQuery.TAG_LAYOUT, scrollState);
+					
+					if(scrollState == SCROLL_STATE_IDLE){
+						
+						AQUtility.debug("idle", view.getChildCount());
+						
+						int first = lv.getFirstVisiblePosition();
+						int last = lv.getLastVisiblePosition();
+						
+						AQUtility.debug(first, last);
+						
+						int count = last - first;
+						
+						//ExpandableListAdapter ela = elv.getExpandableListAdapter();
+						ListAdapter la = lv.getAdapter();
+						
+						for(int i = 0; i <= count; i++){
+						
+							//long packed = elv.getExpandableListPosition(i + first);
+							long packed = i + first;
+							
+							
+							//int group = ExpandableListView.getPackedPositionGroup(packed);
+							//int child = ExpandableListView.getPackedPositionChild(packed);
+							
+							//AQUtility.debug(group,child);
+							
+							//if(group >= 0){
+								
+								View convertView = lv.getChildAt(i);
+								Long targetPacked = (Long) convertView.getTag(AQuery.TAG_LAYOUT);
+								
+								if(targetPacked != null && targetPacked.longValue() == packed){
+								
+									la.getView((int) packed, convertView, lv);
+									/*
+									if(child == -1){
+									
+										ela.getGroupView(group, elv.isGroupExpanded(group), convertView, elv);
+										
+									}else{
+										
+										ela.getChildView(group, child, child == ela.getChildrenCount(group) - 1, convertView, elv);
+										
+									}
+									*/
+									
+									convertView.setTag(AQuery.TAG_LAYOUT, null);
+								}else{
+									AQUtility.debug("skip!");
+								}
+								
+							//}
+						
+								
+						}
+						
+						
+						
+					}
+					
+				}
+				
+				@Override
+				public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+					
+				}
+			};
+			
+			
+		
+			lv.setOnScrollListener(sl);
+			parent.setTag(AQuery.TAG_SCROLL_LISTENER, sl);
+		}
+		
+		Integer scrollState = (Integer) parent.getTag(AQuery.TAG_LAYOUT);
+		
+		if(scrollState == null || scrollState.intValue() == OnScrollListener.SCROLL_STATE_IDLE){
+			return false;
+		}
+		
+		//long packed = ExpandableListView.getPackedPositionForChild(groupPosition, childPosition);
+		long packed = position;
+		convertView.setTag(AQuery.TAG_LAYOUT, packed);
+		
+		return true;
+	}
+	
+	
+	
 	public void renderPhotos(String url, XmlDom xml, AjaxStatus status) {
 	
 		if(xml == null) return;
@@ -117,11 +231,12 @@ public class ImageLoadingList4Activity extends ImageLoadingListActivity {
 				
 				Bitmap placeholder = aq.getCachedImage(R.drawable.image_ph);
 				
-				if(aq.shouldDelay(convertView, parent, tbUrl, 0)){
-					//AQUtility.debug("delay");
+				//if(aq.shouldDelay(convertView, parent, tbUrl, 0)){
+				if(shouldDelay(position, convertView, parent, tbUrl)){
+							
 					aq.id(R.id.tb).image(placeholder);
 				}else{
-					//AQUtility.debug("not delay");
+					
 					aq.id(R.id.tb).image(tbUrl, true, true, 0, R.drawable.image_missing, placeholder, AQuery.FADE_IN_NETWORK, 0);
 				}
 				
