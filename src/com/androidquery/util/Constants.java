e071d1bd8823a8f9694334b6ac28fee698196e72
peter <tinyeeliu@gmail.com>
2012-08-30 15:49:07 +0800

    Update version code.

--- src/com/androidquery/util/Constants.java
+++ src/com/androidquery/util/Constants.java
@@ -19,7 +19,7 @@ package com.androidquery.util;
 public interface Constants {
 
 
-	public static final String VERSION = "0.23.12";
+	public static final String VERSION = "0.23.13";
 	
 	public static final int LAYER_TYPE_SOFTWARE = 1;
 	public static final int LAYER_TYPE_HARDWARE = 2;
