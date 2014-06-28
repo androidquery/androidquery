4df655c1e647263e02b283cf7b3307d9b3e17c55
Greg Ennis <grennis@gmail.com>
2014-06-27 23:25:21 -0400

    Add support for HTTP PATCH method

--- src/com/androidquery/util/Constants.java
+++ src/com/androidquery/util/Constants.java
@@ -44,7 +44,8 @@ public interface Constants {
 	public static final int METHOD_DELETE = 2;
 	public static final int METHOD_PUT = 3;
 	public static final int METHOD_DETECT = 4;
-	
+    public static final int METHOD_PATCH = 5;
+
 	public static final int TAG_URL = 0x40FF0001;
 	public static final int TAG_SCROLL_LISTENER = 0x40FF0002;
 	public static final int TAG_LAYOUT = 0x40FF0003;
---
+++ src/com/androidquery/util/HttpPatch.java
@@ -0,0 +1,16 @@
+package com.androidquery.util;
+
+import org.apache.http.client.methods.HttpPost;
+
+public class HttpPatch extends HttpPost {
+    public static final String METHOD_PATCH = "PATCH";
+
+    public HttpPatch(final String url) {
+        super(url);
+    }
+
+    @Override
+    public String getMethod() {
+        return METHOD_PATCH;
+    }
+}
